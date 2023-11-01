cffbe5f0666804a0d7e9c7607bd87f57788cc4a4
quadrman <kykyshkaigroman@gmail.com>
2023-11-01 20:56:32 +0900

    Add Project files

---
+++ PR#3/While5_For18_Tombasova_Demyanchik/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace While5_For18_Tombasova_Demyanchik
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Задание.Дано вещественное число A и целое число N (> 0). Используя один цикл, найти значение выражения");
+            Console.WriteLine("Введите число X");
+            float X = float.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число N: ");
+            int N = int.Parse(Console.ReadLine());
+            double sum = 0;
+            if (N < 0)
+            {
+                Console.WriteLine("Неправильно ввели число, повторите попытку.");
+                Main();
+            }
+            else
+            {
+                for (int i = 0; i <= N; i++)
+                {
+                    sum += Math.Pow(-X, i);
+                }
+                Console.WriteLine(sum);
+                Console.WriteLine();
+                While8.While();
+            }
+        }
+    }
+}
---
+++ PR#3/While5_For18_Tombasova_Demyanchik/While8.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While5_For18_Tombasova_Demyanchik
+{
+    public class While8
+    {
+        public static void While()
+        {
+            Console.WriteLine("Задание While5.Дано целое число N (> 0), являющееся некоторой степенью числа 2: N = 2K. Найти целое число K — показатель этой степени.");
+            Console.WriteLine("Введите число N, являющееся некоторой степенью числа 2");
+            float N = float.Parse(Console.ReadLine());
+            int k = 0;
+            if (N < 0)
+            {
+                Console.WriteLine("Неправильно ввели число, повторите попытку.");
+                While();
+            }
+            else
+            {
+                while(N != 1)
+                {
+                    N /= 2;
+                    k++;
+                }
+                Console.WriteLine("Степень данного числа равна: "+k.ToString());
+            }
+
+        }
+    }
+}
